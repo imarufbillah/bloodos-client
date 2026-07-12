cf1db6ddfb11197756db93f5a580280c0927849a
Maruf Billah <marufbillah.dev@gmail.com>
2026-07-12 17:58:29 +0600

    feat(dto): centralize exports for all DTOs

---
+++ src/types/dto/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Central export for all DTOs
+ */
+
+// Request DTOs
+export * from "./request.dto";
+
+// Response DTOs
+export * from "./response.dto";
+
+// Donor DTOs
+export * from "./donor.dto";
+
+// Notification DTOs
+export * from "./notification.dto";
+
+// User DTOs
+export * from "./user.dto";
+
+// Donation DTOs
+export * from "./donation.dto";
+
+// Admin DTOs
+export * from "./admin.dto";
+
+// Contact DTOs
+export * from "./contact.dto";
