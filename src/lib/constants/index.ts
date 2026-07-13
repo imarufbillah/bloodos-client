f5413543edcfec50ac650e26e49f655f5e51355b
Maruf Billah <marufbillah.dev@gmail.com>
2026-07-13 13:13:01 +0600

    feat(constants): add barrel exports for shared constants

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export point for all BloodOS constants
+ * These mirror the server-side enums and data structures
+ */
+
+export * from "./bloodGroups";
+export * from "./districts";
+export * from "./compatibility";
