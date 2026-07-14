285c8264398855a4eebe388336eb409f29e09ec1
Maruf Billah <marufbillah.dev@gmail.com>
2026-07-15 00:07:09 +0600

    refactor(auth-client): export signOut method from authClient for better access

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL as string,
   plugins: [jwtClient()],
 });
 
-export const { signIn, signUp, useSession } = createAuthClient();
+export const { signIn, signUp, useSession, signOut } = authClient;
